ba3c22a2c64451c12cbb760007e7415da7ffbfb3
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-09-01 10:07:16 +0200

    feat: implement routing with react-router-dom in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
 
 
   return (
     <>
-      Progetto finale
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>'Home'</p>} />
+          <Route path="/plant/:id" element={<p>'Dettaglio'</p>} />
+          <Route path="*" element={<p>Not Found</p>} />
+        </Routes>
+      </Router>
     </>
   )
 }
