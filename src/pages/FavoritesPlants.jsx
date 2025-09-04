256f44fbbf5e6a9bb7e901a5e6f88562d6c7c040
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-09-04 09:30:37 +0200

    feat: update empty list styling and refactor FavoritesPlants component for improved layout

--- src/pages/FavoritesPlants.jsx
+++ src/pages/FavoritesPlants.jsx
@@ -6,19 +6,13 @@ function FavoritesPlants() {
     //destrutturo per ottenere ciò che mi serve per la gestione dei preferiti
     const { favorites, toggleFavorite } = useFavorites();
 
-    if (favorites.length === 0) {
-        return <div className="empty-list">
-            <p >La tua lista è vuota. </p>
-            <img src="/icons/flower.png" alt="flower" />
-        </div>
-    }
 
     return (
         <>
             <h1 className="plant-list-title">Le tue piante preferite! <img src="/icons/plant-love.png" alt="plant-love" /> </h1>
 
             <ul className="plant-list-display">
-                {favorites.map(plant =>
+                {favorites.length > 0 ? (favorites.map(plant =>
                     <li className="plant-list-el" key={plant.id}>
                         <PlantCard
                             data={plant}
@@ -26,8 +20,11 @@ function FavoritesPlants() {
                             onToggleFavorite={toggleFavorite}
                             showSelect={false}
                         />
-                    </li>
-                )}
+                    </li>)) : <div className="empty-list">
+                    <p >La tua lista è vuota. </p>
+                    <img src="/icons/flower.png" alt="flower" />
+                </div>
+                }
             </ul>
 
         </>
