7d8eac809d782803a94d187a5e99f6d7451b7c49
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-09-05 11:13:09 +0200

    feat: optimize ComparePlants component with useMemo for rendering plant comparison list

--- src/pages/ComparePlants.jsx
+++ src/pages/ComparePlants.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import PlantCardDetail from "../components/PlantCardDetail";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useFavorites } from "../context/FavouritesContext";
 
 function ComparePlants() {
@@ -37,7 +37,7 @@ function ComparePlants() {
                 );
                 console.log('Array di elementi selezionati', data);
                 setPlants(data);
-                console.log('plants è =', plants)
+
             } catch (error) {
                 console.error('Errore nel recupero dati delle piante', error);
             }
@@ -46,21 +46,26 @@ function ComparePlants() {
 
         selectedPlantsFetch();
 
-    }, [ids, navigate])
+    }, [ids, navigate]);
+
+    // ottimizzazione della lista comparativa con useMemo
+    const compareList = useMemo(() => {
+
+        return plants.map((p, index) => <PlantCardDetail key={p.id ?? index}
+            data={p}
+            isFavorite={favorites.some(fav => p.id === fav.id)}
+            onToggleFavorite={toggleFavorite}
+        />
+
+        )
+    }, [plants, favorites, toggleFavorite]);
 
     return (
         <div>
             <h2 className="plant-list-title">Confronta le piante! <img src="/icons/compare.png" alt="compare" /></h2>
 
-
             <div className={`compare-list-container ${plants.length === 4 ? 'four-compare-items' : ''}`}>
-
-                {plants.length > 0 ? plants.map((p, index) => <PlantCardDetail key={p.id ?? index}
-                    data={p}
-                    isFavorite={favorites.some(fav => p.id === fav.id)}
-                    onToggleFavorite={toggleFavorite}
-                />) : <p>Comparatore vuoto!</p>}
-
+                {plants.length > 0 ? compareList : <p>Comparatore vuoto!</p>}
             </div>
 
             <button className="btn-sort-items margin-bottom" onClick={() => navigate(-1)}>Torna indietro</button>
