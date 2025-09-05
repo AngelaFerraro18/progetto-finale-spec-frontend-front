a464ffccfeb51e050c7b26ef0bce1ce3184b9db8
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-09-05 11:08:53 +0200

    feat: optimize FavoritesPlants component with useMemo for rendering favorites list

--- src/pages/FavoritesPlants.jsx
+++ src/pages/FavoritesPlants.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import PlantCard from "../components/PlantCard";
 import { useFavorites } from "../context/FavouritesContext";
 
@@ -6,26 +7,31 @@ function FavoritesPlants() {
     //destrutturo per ottenere ciò che mi serve per la gestione dei preferiti
     const { favorites, toggleFavorite } = useFavorites();
 
+    // ottimizzazione della lista dei preferiti con useMemo
+    const favoritesList = useMemo(() => {
+        return favorites.map(plant =>
+            <li className="plant-list-el" key={plant.id}>
+                <PlantCard
+                    data={plant}
+                    isFavorite={true}
+                    onToggleFavorite={toggleFavorite}
+                    showSelect={false}
+                />
+            </li>)
+    }, [favorites, toggleFavorite]);
+
 
     return (
         <>
             <h1 className="plant-list-title">Le tue piante preferite! <img src="/icons/plant-love.png" alt="plant-love" /> </h1>
 
             <ul className="plant-list-display">
-                {favorites.length > 0 ? (favorites.map(plant =>
-                    <li className="plant-list-el" key={plant.id}>
-                        <PlantCard
-                            data={plant}
-                            isFavorite={true}
-                            onToggleFavorite={toggleFavorite}
-                            showSelect={false}
-                        />
-                    </li>)) : <div className="empty-list">
+                {favorites.length > 0 ? favoritesList : <div className="empty-list">
                     <p >La tua lista è vuota. </p>
                     <img src="/icons/flower.png" alt="flower" />
-                </div>
+                </div >
                 }
-            </ul>
+            </ul >
 
         </>
     )
